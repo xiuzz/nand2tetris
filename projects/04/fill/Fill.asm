c0dd9b2e12abadd7865ee78449b5430d4f0e7715
xiuzz <1519072696@qq.com>
2023-10-10 00:49:38 +0800

    lesson3 half-final

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,9 +1,9 @@
 // This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
+// and the book "The Elements of Computing Systems"e4w
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input.
+// Runs an i)[finite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
 // the screen should remain fully black as long as the key is pressed. 
@@ -12,3 +12,6 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+@SCREEN
+
+M=1
